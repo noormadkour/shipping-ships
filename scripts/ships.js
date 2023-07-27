358a3b8369a5a1baf13ad9937917dbdbdf607aee
Noor Madkour <noormadkour@gmail.com>
2023-07-27 15:28:47 -0500

    renamed src to scripts

---
+++ scripts/ships.js
@@ -0,0 +1,13 @@
+import { getShips } from "./database.js";
+
+export const shipList = () => {
+  const ships = getShips();
+  let shipHTML = `<ul>`;
+  for (const ship of ships) {
+    shipHTML += `<li data-type="ship"
+                      data-id="${ship.id}>
+Ship #${ship.id} is named ${ship.shipName}, and is being hauled by ${ship.hauler}</li>`;
+  }
+  shipHTML += `</ul>`
+  return shipHTML;
+};
