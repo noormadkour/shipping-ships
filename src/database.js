b6d5c99db0ae879feeb83c47247faf219ed9ab8b
Noor Madkour <noormadkour@gmail.com>
2023-07-27 15:07:39 -0500

    added haulers to database and make a gethaulers function

--- src/database.js
+++ src/database.js
@@ -5,8 +5,46 @@ const database = {
     { id: 3, location: "Rotterdam, The Netherlands", volume: "14.35" },
     { id: 4, location: "Antwerp, Belgium", volume: "12.04" },
   ],
+  haulers: [
+    {
+      id: 1,
+      haulerName: "Haulin' Shanghai Surprise",
+      dock_location: "Shanghai, China",
+    },
+    {
+      id: 2,
+      haulerName: "Busan the Move",
+      dock_location: "Busan, South Korea",
+    },
+    {
+      id: 3,
+      haulerName: "Rockin' Rotterdam",
+      dock_location: "Rotterdam, The Netherlands",
+    },
+    {
+      id: 4,
+      haulerName: "Antwerp-tunity Knocks",
+      dock_location: "Antwerp, Belgium",
+    },
+    { id: 5, haulerName: "Seas the Day", dock_location: "Antwerp, Belgium" },
+    { id: 6, haulerName: "Knot on Call", dock_location: "Shanghai, China" },
+    {
+      id: 7,
+      haulerName: "Anchorman",
+      dock_location: "Rotterdam, The Netherlands",
+    },
+    {
+      id: 8,
+      haulerName: "The Salty Mermaid",
+      dock_location: "Busan, South Korea",
+    },
+  ],
 };
 
 export const getDocks = () => {
   return database.docks.map((dock) => ({ ...dock }));
+};
+
+export const getHaulers = () => {
+  return database.haulers.map((hauler) => ({ ...hauler }));
 };
--- src/docks.js
+++ src/docks.js
@@ -1,12 +1,13 @@
 import { getDocks } from "./database.js";
 
 export const dockList = () => {
-    const docks = getDocks();
-    let docksHTML = `<ul>`;
-    for (const dock of docks){
-        docksHTML += `<li data-type="dock"
-                          data-id="${dock.id}
-> Dock #${dock.id} is located in ${dock.location}, and can hold ${dock.volume} million tons of cargo</li>`
-    }
-
-}
+  const docks = getDocks();
+  let docksHTML = `<ul>`;
+  for (const dock of docks) {
+    docksHTML += `<li data-type="dock"
+                      data-id="${dock.id}>
+Dock #${dock.id} is located in ${dock.location}, and can hold ${dock.volume} million tons of cargo</li>`;
+  }
+  docksHTML += `</ul>`
+  return docksHTML;
+};
