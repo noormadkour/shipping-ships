759cd8ba6d86354da4ebf849dd6fb897fd2ef6c6
Noor Madkour <noormadkour@gmail.com>
2023-08-07 13:24:46 -0500

    not sure what changes just wanna get rid of that silly x

--- scripts/main.js
+++ scripts/main.js
@@ -3,10 +3,10 @@ import { haulerList } from "./haulers.js";
 import { shipList } from "./ships.js";
 
 const dockContainer = document.querySelector(".docks");
-dockContainer.innerHTML = dockList();
-
 const haulerContainer = document.querySelector(".haulers");
-haulerContainer.innerHTML = haulerList();
-
 const shipContainer = document.querySelector(".ships");
+
+dockContainer.innerHTML = dockList();
+haulerContainer.innerHTML = haulerList();
 shipContainer.innerHTML = shipList();
+
